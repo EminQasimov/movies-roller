1477e3f78a4588b90320d15ced670b1dc3023603
Emin Qasimov <eminqasimov@outlook.com>
2019-03-29 17:19:14 +0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# movies-roller
-Movie Roller Slider with CSS 3D Transforms
---
+++ precache-manifest.9efa0c499cf57b3e4f7beb4f904e6700.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4679eae8e98bb124c8b8fc17a230156",
+    "url": "/movies-roller/static/media/harrypotter.f4679eae.jpg"
+  },
+  {
+    "revision": "c56228a6400deaaa53c9",
+    "url": "/movies-roller/static/css/main.769086c8.chunk.css"
+  },
+  {
+    "revision": "31de2eed511612221aac",
+    "url": "/movies-roller/static/js/runtime~main.48a47607.js"
+  },
+  {
+    "revision": "53d842fbe433844137b0",
+    "url": "/movies-roller/static/js/2.3829dcdb.chunk.js"
+  },
+  {
+    "revision": "a3cf144f486d5656e8a2d9756d2a789e",
+    "url": "/movies-roller/static/media/venom.a3cf144f.jpg"
+  },
+  {
+    "revision": "2ab182174e327c0405db9346bdb9f284",
+    "url": "/movies-roller/static/media/airbender.2ab18217.jpg"
+  },
+  {
+    "revision": "09b82709c1a48ebdd3eecde6d6c79942",
+    "url": "/movies-roller/static/media/aliens.09b82709.jpg"
+  },
+  {
+    "revision": "e437e8468dc3aa31d1dbd995c140242d",
+    "url": "/movies-roller/static/media/assassin.e437e846.jpg"
+  },
+  {
+    "revision": "2820b2783e3f8e317210c737b0834432",
+    "url": "/movies-roller/static/media/escape.2820b278.jpg"
+  },
+  {
+    "revision": "e2e4ffc5e347660b927d8491c03c862f",
+    "url": "/movies-roller/static/media/flash.e2e4ffc5.jpg"
+  },
+  {
+    "revision": "c56228a6400deaaa53c9",
+    "url": "/movies-roller/static/js/main.5c9a5b22.chunk.js"
+  },
+  {
+    "revision": "c7c2b6191be6c3a46d8c3e944b186959",
+    "url": "/movies-roller/static/media/ironman.c7c2b619.jpg"
+  },
+  {
+    "revision": "b23562e75b6e6321d0722d574538a50a",
+    "url": "/movies-roller/static/media/jonuik.b23562e7.jpg"
+  },
+  {
+    "revision": "ee284199b0b7bcc4d4441bca531b7af9",
+    "url": "/movies-roller/static/media/robinhood.ee284199.jpg"
+  },
+  {
+    "revision": "7ac79199a0ceb3fb608b01db7eff74fc",
+    "url": "/movies-roller/static/media/spiderman.7ac79199.jpg"
+  },
+  {
+    "revision": "8e5d98c838b612d3122d83403cbb5777",
+    "url": "/movies-roller/static/media/starwar.8e5d98c8.jpg"
+  },
+  {
+    "revision": "6af1cd2b4de6f060fa119191950c047f",
+    "url": "/movies-roller/static/media/superman.6af1cd2b.jpg"
+  },
+  {
+    "revision": "c0e8c4b6e9859382bc81dce82a367003",
+    "url": "/movies-roller/static/media/thor.c0e8c4b6.jpg"
+  },
+  {
+    "revision": "bc96cb8b716c3bf42cb792add057400a",
+    "url": "/movies-roller/static/media/valerian.bc96cb8b.jpg"
+  },
+  {
+    "revision": "81083a3b7ef0fa3d16712c300726e21e",
+    "url": "/movies-roller/index.html"
+  }
+];
